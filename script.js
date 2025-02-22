5567f9c3414f89771849b13204a68047b1a9eb68
billpsem <billpsem@gmail.com>
2025-02-22 18:11:58 +0200

    removed the js content

--- script.js
+++
@@ -1,5 +0,0 @@
-function changeBackgroundColor() {
-    let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = color;
-    document.body.style.color = (parseInt(color.substring(1), 16) > 0xffffff / 2) ? '#000' : '#fff';
-}
